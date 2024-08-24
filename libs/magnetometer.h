d4aac18d794456253561f312a9a7e407b5fcaf86
Cody <cj.allen4131@gmail.com>
2024-08-24 12:55:06 -0400

    Began getting magnetometer connected

---
+++ libs/magnetometer.h
@@ -0,0 +1,15 @@
+#ifndef _MAGNETOMETER
+#define _MAGNETOMETER
+
+
+#define MAG_REG 0X0d
+
+// Magnetometer registers
+#define CONFIG_A 0x00
+#define CONFIG_B 0x01
+#define MODE_REG 0X02
+#define MAG_READINGS 0X03
+#define STATUS_REG 0X09
+#define ID_REG 0X0a
+
+#endif
---
+++ src/magnetometer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+
+#include "../libs/mpu6050.h"
+#include "../libs/i2c_config.h"
+
+static void mag_reset() {
+
+
+
+}
